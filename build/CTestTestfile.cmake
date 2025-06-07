db9c71831fd58162f7ec3d55b83a4511397228fb
bushuevasofia <bushuevasofia@mail.ru>
2025-06-07 15:44:54 -0400

    add changed

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kali/workspace/projects/lab05
-# Build directory: /home/kali/workspace/projects/lab05/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(my_test "/home/kali/workspace/projects/lab05/build/my_test")
-set_tests_properties(my_test PROPERTIES  _BACKTRACE_TRIPLES "/home/kali/workspace/projects/lab05/CMakeLists.txt;33;add_test;/home/kali/workspace/projects/lab05/CMakeLists.txt;0;")
-subdirs("third-party/gtest")
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -5,72 +5,128 @@
 #include "Transaction.h"
 
 using ::testing::Return;
-using ::testing::Throw;
 using ::testing::_;
+using ::testing::AtLeast;
 
+// Mock класс для Account
 class MockAccount : public Account {
 public:
     MockAccount(int id, int balance) : Account(id, balance) {}
-    MOCK_CONST_METHOD0(GetBalance, int());
-    MOCK_METHOD1(ChangeBalance, void(int diff));
-    MOCK_METHOD0(Lock, void());
-    MOCK_METHOD0(Unlock, void());
+    MOCK_METHOD(int, GetBalance, (), (const, override));
+    MOCK_METHOD(void, ChangeBalance, (int), (override));
+    MOCK_METHOD(void, Lock, (), (override));
+    MOCK_METHOD(void, Unlock, (), (override));
 };
 
-TEST(AccountTest, LockUnlockBehaviour) {
-    MockAccount acc(0, 1000);
-    EXPECT_CALL(acc, Lock()).Times(2);
-    EXPECT_CALL(acc, Unlock()).Times(1);
-    acc.Lock();
-    acc.Lock();
-    acc.Unlock();
+// Тесты для Account
+TEST(Account, GetBalance_ReturnsCorrectValue) {
+    Account account(1, 1000);
+    EXPECT_EQ(account.GetBalance(), 1000);
 }
 
-TEST(AccountTest, BalanceModification) {
-    Account acc(0, 1000);
-    acc.Lock();
-    acc.ChangeBalance(100);
-    EXPECT_EQ(acc.GetBalance(), 1100);
+TEST(Account, ChangeBalance_UpdatesBalance) {
+    Account account(1, 1000);
+    account.Lock();
+    account.ChangeBalance(500);
+    EXPECT_EQ(account.GetBalance(), 1500);
 }
 
-TEST(TransactionTest, MakeTransactionSuccess) {
-    Account from(0, 6132);
-    Account to(1, 2133);
-    Transaction tx;
-    tx.set_fee(32);
-
-    bool result = tx.Make(from, to, 100);
-    EXPECT_TRUE(result);
-   
+TEST(Account, Lock_Unlock_WorksCorrectly) {
+    Account account(1, 1000);
+    account.Lock();
+    EXPECT_THROW(account.Lock(), std::runtime_error);
+    account.Unlock();
+    EXPECT_NO_THROW(account.Lock());
 }
 
-// Исправленный вариант тестов для текущей реализации Transaction
-TEST(TransactionTest, CorrectedSuccessfulTransfer) {
-    Account from(0, 6132);
-    Account to(1, 2133);
-    Transaction tx;
-    tx.set_fee(32);
+// Тесты для Transaction
+TEST(Transaction, Make_CallsAccountMethodsCorrectly) {
+    // Создаем mock-объекты для аккаунтов
+    MockAccount from(1, 1000);
+    MockAccount to(2, 500);
 
-    bool result = tx.Make(from, to, 100);
-    EXPECT_TRUE(result);
-    
-    // Проверяем только то, что гарантирует текущая реализация:
-    EXPECT_NE(from.GetBalance(), 6132); // Баланс изменился
-    EXPECT_NE(to.GetBalance(), 2133);   // Баланс изменился
+    // Настраиваем ожидания для методов
+    EXPECT_CALL(from, Lock()).Times(1);
+    EXPECT_CALL(to, Lock()).Times(1);
+    EXPECT_CALL(from, Unlock()).Times(1);
+    EXPECT_CALL(to, Unlock()).Times(1);
+    EXPECT_CALL(from, ChangeBalance(-110)).Times(1);
+    EXPECT_CALL(to, ChangeBalance(100)).Times(1);
+    EXPECT_CALL(from, GetBalance()).Times(AtLeast(1)).WillRepeatedly(Return(1000));
+    EXPECT_CALL(to, GetBalance()).Times(AtLeast(1)).WillRepeatedly(Return(500));
+
+    // Выполняем транзакцию
+    Transaction transaction;
+    transaction.set_fee(10);
+    EXPECT_TRUE(transaction.Make(from, to, 100));
 }
 
-TEST(TransactionTest, InvalidTransfers) {
-    Transaction tx;
-    tx.set_fee(51);
-    Account same(0, 1000);
-    Account poor(1, 10);
-    Account rich(2, 1000);
+TEST(Transaction, Make_ThrowsExceptionIfSumIsNegative) {
+    MockAccount from(1, 1000);
+    MockAccount to(2, 500);
 
-    EXPECT_THROW(tx.Make(same, same, 100), std::logic_error);
-    EXPECT_THROW(tx.Make(rich, poor, -100), std::invalid_argument);
-    EXPECT_THROW(tx.Make(rich, poor, 50), std::logic_error);
-    EXPECT_FALSE(tx.Make(rich, poor, 100));
-    
-    tx.set_fee(10);
-    EXPECT_FALSE(tx.Make(poor, rich, 100));
+    Transaction transaction;
+    EXPECT_THROW(transaction.Make(from, to, -100), std::invalid_argument);
+}
+
+TEST(Transaction, Make_ThrowsExceptionIfSumIsTooSmall) {
+    MockAccount from(1, 1000);
+    MockAccount to(2, 500);
+
+    Transaction transaction;
+    EXPECT_THROW(transaction.Make(from, to, 50), std::logic_error);
+}
+
+TEST(Transaction, Make_ReturnsFalseIfFeeIsTooHigh) {
+    MockAccount from(1, 1000);
+    MockAccount to(2, 500);
+
+    Transaction transaction;
+    transaction.set_fee(1000);
+    EXPECT_FALSE(transaction.Make(from, to, 100));
+}
+
+TEST(Transaction, Make_SavesToDatabaseCorrectly) {
+    MockAccount from(1, 1000);
+    MockAccount to(2, 500);
+
+    // Настраиваем ожидания для всех вызовов методов
+    EXPECT_CALL(from, Lock()).Times(1);
+    EXPECT_CALL(to, Lock()).Times(1);
+    EXPECT_CALL(from, Unlock()).Times(1);
+    EXPECT_CALL(to, Unlock()).Times(1);
+    EXPECT_CALL(from, ChangeBalance(_)).Times(1);
+    EXPECT_CALL(to, ChangeBalance(_)).Times(1);
+    EXPECT_CALL(from, GetBalance()).Times(AtLeast(1)).WillRepeatedly(Return(1000));
+    EXPECT_CALL(to, GetBalance()).Times(AtLeast(1)).WillRepeatedly(Return(500));
+
+    // Выполняем транзакцию
+    Transaction transaction;
+    transaction.set_fee(10);
+    EXPECT_TRUE(transaction.Make(from, to, 100));
+}
+
+TEST(Transaction, Make_ReturnsFalseIfInsufficientFunds) {
+    MockAccount from(1, 100); // На счету 100
+    MockAccount to(2, 500);   // На счету 500
+
+    // Настраиваем ожидания для вызовов
+    EXPECT_CALL(from, Lock()).Times(1);
+    EXPECT_CALL(to, Lock()).Times(1);
+    EXPECT_CALL(from, Unlock()).Times(1);
+    EXPECT_CALL(to, Unlock()).Times(1);
+    EXPECT_CALL(from, GetBalance())
+        .WillOnce(Return(100))  // Первый вызов
+        .WillOnce(Return(100)); // Второй вызов
+    EXPECT_CALL(to, GetBalance())
+        .WillOnce(Return(500)); // Первый вызов
+    EXPECT_CALL(to, ChangeBalance(200)).Times(1);           // Вызов ChangeBalance для получателя
+    EXPECT_CALL(from, ChangeBalance(-200 - 10)).Times(0);   // Не должно быть вызова для отправителя
+    EXPECT_CALL(to, ChangeBalance(-200)).Times(1);          // Откат ChangeBalance для получателя
+
+    Transaction transaction;
+    transaction.set_fee(10); // Устанавливаем комиссию 10
+
+    // Пытаемся перевести 200, что больше, чем на счету
+    EXPECT_FALSE(transaction.Make(from, to, 200));
 }
